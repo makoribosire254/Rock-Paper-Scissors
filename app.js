985fa22bf90fdde8710f0882855e911a80132800
Kopite007 <makoribosire254@gmail.com>
2024-02-25 12:36:19 +0300

    Adding odin.html and app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+// let name = 'Tyra Kerubo';
+// console.log(`Hello ${name}`);
+
+function getComputerChoice(){
+  let choice = Math.floor(Math.random() * 3)
+  console.log(choice);
+}
+
+getComputerChoice();
