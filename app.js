369eaec9afb87128e4dc20d465c42cde8cc751e7
Kopite007 <makoribosire254@gmail.com>
2024-02-25 13:24:24 +0300

    Modifying ap.js with function getComputerChoice()

--- app.js
+++ app.js
@@ -2,8 +2,22 @@
 // console.log(`Hello ${name}`);
 
 function getComputerChoice(){
-  let choice = Math.floor(Math.random() * 3)
-  console.log(choice);
-}
+  // let computerChoice = Math.floor(Math.random() * 3)
+  // if (computerChoice === 0) {
+  //   computerChoice === 'rock'
+  //   console.log('rock');
+  // } else if (computerChoice === 1) {
+  //   computerChoice === 'paper'
+  //   console.log('paper');
+  // } else {
+  //   computerChoice === 'scissors';
+  //   console.log('scissors');
 
-getComputerChoice();
+  let computerChoice = ['rock', 'paper', 'scissors'];
+  let randomInt = Math.floor(Math.random() * computerChoice.length);
+
+  return computerChoice[randomInt];
+  } 
+
+const computerSelection = getComputerChoice();
+console.log(`Computer choice's: ${computerSelection}`);
